1fe12362913e36bfe64281315982cc31f1fb5764
Ram Prakash V <naruto402002@gmail.com>
2025-02-01 17:01:30 +0530

    day 1

--- wipro_java_project/src/com/wipro/java/Flowers.java
+++ wipro_java_project/src/com/wipro/java/Flowers.java
@@ -23,9 +23,11 @@ public class Flowers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Flowers flowerobject1=new  Flowers("Rose","Red");
-		Flowers flowerobject2=new  Flowers("Sunflower","Marigold");
+		Flowers flowerobject2=new  Flowers("Sunflower","yellow");
+		Flowers flowerobject3=new  Flowers("lily","white");
 		flowerobject1.display();
 		flowerobject2.display();
+		flowerobject3.display();
 		
 
 	}
