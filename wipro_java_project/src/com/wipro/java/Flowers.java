e82886fe24fedec8c8221cd3d4066ad2d1e4bc66
Ram Prakash V <naruto402002@gmail.com>
2025-02-01 16:14:41 +0530

    commit

--- wipro_java_project/src/com/wipro/java/Flowers.java
+++ wipro_java_project/src/com/wipro/java/Flowers.java
@@ -6,12 +6,12 @@ package com.wipro.java;
 /**
  * 
  */
-public class HelloWorld {
+public class Flowers {
 
 	/**
 	 * 
 	 */
-	public HelloWorld() {
+	public Flowers() {
 		// TODO Auto-generated constructor stub
 	}
 
@@ -20,7 +20,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My First Java");
+
 	}
 
 }
